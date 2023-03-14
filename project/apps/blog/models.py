157aa805d8d2304410e51dec8e8a53d382c44aa9
rovsen762 <rovsen.memmedli.762@gmail.com>
2023-03-14 23:37:31 +0400

    This is third commit that where I added urls and views to app.

--- project/apps/blog/models.py
+++ project/apps/blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+
+class PublishedManager(models.Manager):
+   def get_queryset(self):
+       return super().get_queryset() \
+           .filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -19,6 +25,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         ordering = ['-publish']
         indexes = [
--- project/apps/blog/views.py
+++ project/apps/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    return render(request,'blog/post/detail.html',{ 'post': post})
