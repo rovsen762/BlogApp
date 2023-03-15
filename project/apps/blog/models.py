d809f10e834635fe09c3d3f0bb297ed19f2f660b
rovsen762 <rovsen.memmedli.762@gmail.com>
2023-03-15 20:32:03 +0400

    New updates by chapter 1.
    1.Added Timestamp file to my project.Timestamp is abstract model for using created and updated fields in other models.

--- project/apps/blog/models.py
+++ project/apps/blog/models.py
@@ -16,14 +16,10 @@ class Post(TimeStamp):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    status = models.CharField(max_length=2,
-                              choices=Status.choices,
-                              default=Status.DRAFT)
+    status = models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
     objects = models.Manager()
     published = PublishedManager()
 
