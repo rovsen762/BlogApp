cb7a1d40a22ed5101b629f276366349e5b917127
rovsen762 <rovsen.memmedli.762@gmail.com>
2023-03-17 00:14:40 +0400

    New updates by chapter 2.
    1.Added get_absolute_url to Blog model.This method allow us to define dinamic url for created object from Blog model.Also I define when post_detail url calling it will blog object from blog.id and I added it to html page.

--- project/apps/blog/models.py
+++ project/apps/blog/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from utils.models.timestamp import TimeStamp
-
-
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
    def get_queryset(self):
@@ -30,5 +29,9 @@ class Post(TimeStamp):
         ]
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.id])
 
 
