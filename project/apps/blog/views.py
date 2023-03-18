ebd4f3b86806d2a2792bd502f16cf48406dff48f
rovsen762 <rovsen.memmedli.762@gmail.com>
2023-03-18 14:41:13 +0400

    New updates by Chapter 2.
    1.I created PostListView (class based view) for list.html.My view inherited from ListView(Django Generic Views).
    2.I configured my url for my class based view.

--- project/apps/blog/views.py
+++ project/apps/blog/views.py
@@ -3,22 +3,12 @@ from .models import Post
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-
-
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    return render(request,
-                  'list.html',
-                  {'posts': posts})
-
+from django.views.generic import ListView
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
